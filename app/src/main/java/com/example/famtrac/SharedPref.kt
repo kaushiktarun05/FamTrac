406cfdcca98cbdb965f710cc51912415565dcd20
Tarun Kaushik <112891230+kaushiktarun05@users.noreply.github.com>
2023-05-03 22:15:51 +0530

    Update SharedPref.kt

--- app/src/main/java/com/example/famtrac/SharedPref.kt
+++ app/src/main/java/com/example/famtrac/SharedPref.kt
@@ -11,17 +11,22 @@ object SharedPref {
 
 
     fun init(context: Context) {
+        
         preferences = context.getSharedPreferences(NAME, MODE)
+        
     }
 
 
     fun putBoolean(key: String, value: Boolean) {
 
         preferences.edit().putBoolean(key, value).commit()
+        
     }
 
     fun getBoolean(key: String): Boolean {
+        
         return preferences.getBoolean(key, false)
+        
     }
 
 
